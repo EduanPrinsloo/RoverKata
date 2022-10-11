ca04427aa955990c9ac9d7691304010b4f28b798
Eduan <eduanp@vatit.com>
2022-10-11 16:23:00 +0200

    feat: get rover input and validate location

--- src/main/kotlin/com/thedevadventure/roverkata/RoverKata.kt
+++ src/main/kotlin/com/thedevadventure/roverkata/RoverKata.kt
@@ -4,13 +4,37 @@ import java.util.Scanner
 
 fun main() {
     val scanner = Scanner(System.`in`)
+//    Get and validate the plateau
     println("Please enter the dimensions of your plateau starting with the size of X?")
     val plateauDimensionsX = scanner.nextInt()
     println("And the size of Y?")
     val plateauDimensionsY = scanner.nextInt()
-    validatePlateau(plateauDimensionsX, plateauDimensionsY)
+    val marsTerrain = Plateau(plateauDimensionsX, plateauDimensionsY)
+    validatePlateau(marsTerrain)
+
+//    Get and validate the locations of the rovers
+    println("Please enter the first rovers coordinates, starting with X hitting enter and then Y...")
+    val firstRoverLocation = Location(scanner.nextInt(),scanner.nextInt())
+    validateTheRoverHasLanded(marsTerrain, firstRoverLocation)
 }
 
-fun validatePlateau(plateauDimensionsX: Int, plateauDimensionsY: Int): Boolean {
-    return plateauDimensionsX > 0 && plateauDimensionsY > 0
+fun validatePlateau(plateauDimensions: Plateau): Boolean {
+    if (plateauDimensions.X > 0 && plateauDimensions.Y > 0) {
+        println("Thank you for passing a valid plateau...")
+        return true
+    } else {
+        throw Exception("Invalid plateau passed both your inputs should be larger than 0!")
+    }
 }
+
+fun validateTheRoverHasLanded(plateau: Plateau, location: Location): Boolean {
+    if ((location.X > plateau.X || location.X < 0) || (location.Y > plateau.Y || location.Y < 0)) {
+        throw Exception("Mission failed... The rover missed the target!")
+    } else {
+        println("Rover has been located on the plateau, good job!")
+        return true
+    }
+}
+
+data class Plateau(val X: Int, val Y: Int)
+data class Location(val X: Int, val Y: Int)
--- src/test/kotlin/com/thedevadventure/roverkata/RoverKataTest.kt
+++ src/test/kotlin/com/thedevadventure/roverkata/RoverKataTest.kt
@@ -1,5 +1,6 @@
 package com.thedevadventure.roverkata
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -8,7 +9,40 @@ internal class RoverKataTest {
     @Test
     fun `should return true if the plateau is valid`() {
         val expected = true
-        val result = validatePlateau(7, 8)
+        val result = validatePlateau(Plateau(7, 8))
         assertEquals(expected, result)
     }
+
+    @Test
+    fun `should throw an error if a invalid dimension is passed`() {
+        try {
+            validatePlateau(Plateau(-1, -2))
+            Assertions.fail("Exception expected")
+        } catch (e: Exception) {
+            assertEquals("Invalid plateau passed both your inputs should be larger than 0!", e.message)
+        }
+    }
+
+    @Test
+    fun `should return value true if the Rovers starting location is on the plateau`() {
+        val plateau = Plateau(5, 5)
+        val validLocation = Location(0, 5)
+        val expected = true
+        val result = validateTheRoverHasLanded(plateau, validLocation)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `should throw an error if the Rovers starting location is NOT on the plateau`() {
+        try {
+            val plateau = Plateau(5, 5)
+            val invalidLocation = Location(-1, 1)
+            validateTheRoverHasLanded(plateau, invalidLocation)
+            Assertions.fail("Exception expected")
+        } catch (e: Exception) {
+            assertEquals("Mission failed... The rover missed the target!", e.message)
+        }
+    }
+
+
 }
